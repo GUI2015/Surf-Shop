d2687256b248c2f5712c92e5c4f5fbd0716df998
Ghanshyam <g.kumbharkar@gmail.com>
2019-01-12 11:56:52 +0530

    Add review routes

--- routes/posts.js
+++ routes/posts.js
@@ -3,22 +3,22 @@ const router = express.Router();
 
 /* GET Posts page. /posts */
 router.get('/', (req, res, next) => {
-	res.send('INDEX/post');
+	res.send('INDEX/posts');
 });
 
 /* GET Posts new. /posts/new */
 router.get('/new', (req, res, next) => {
-	res.send('NEW /post/new');
+	res.send('NEW /posts/new');
 });
 
 /* POST Posts create. /posts */
 router.get('/', (req, res, next) => {
-	res.send('CREATE /post');
+	res.send('CREATE /posts');
 });
 
 /* GET Posts show. /posts/:id */
 router.get('/:id', (req, res, next) => {
-	res.send('SHOW /post/:id');
+	res.send('SHOW /posts/:id');
 });
 
 /* GET Posts Edit. /posts/:id/edit */
@@ -28,12 +28,12 @@ router.get('/:id/edit', (req, res, next) => {
 
 /* PUT Posts update. /posts/:id */
 router.put('/:id', (req, res, next) => {
-	res.send('UPDATE /post/:id');
+	res.send('UPDATE /posts/:id');
 });
 
 /* DELETE Posts destroy. /posts/:id */
 router.delete('/:id', (req, res, next) => {
-	res.send('DELETE /post/:id');
+	res.send('DELETE /posts/:id');
 });
 
 module.exports = router;
