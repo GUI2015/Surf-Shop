ab15c882dda2a507e21eb8e8ea2fb996b3bbb081
Ghanshyam <g.kumbharkar@gmail.com>
2019-01-30 10:18:24 +0530

    add user model and authentication

--- app.js
+++ app.js
@@ -3,12 +3,31 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
+const User = require('./models/user');
+const session = require('express-session');
+const mongoose = require('mongoose');
 
+//require Routes
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
 
 const app = express();
+
+//connect to the database
+mongoose.connect(
+	'mongodb://Ghanshyam:June2015@ds241664.mlab.com:41664/surf-shop',
+	{ useNewUrlParser: true }
+);
+
+mongoose.set('useCreateIndex', true);
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log(`we're connected!`);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,10 +39,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Configure Passport and Sessions
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+
+app.use(
+	session({
+		secret: 'hi there',
+		resave: false,
+		saveUninitialized: true
+	})
+);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Mount Route
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/posts/:id/reviews', reviewsRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
